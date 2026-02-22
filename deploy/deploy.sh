833617746cb3ba23d638d9bda4289263b6566822
Sahil Jagtap <sahil.jagtap45@gmail.com>
2026-02-21 20:32:30 -0500

    Specify platform for Docker build to ensure compatibility

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -20,7 +20,7 @@ echo ""
 echo "=== 2. Build and push Docker image ==="
 aws ecr get-login-password --region $REGION \
   | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-docker build -t $REPO_NAME $DIR
+docker build --platform linux/amd64 -t $REPO_NAME $DIR
 docker tag $REPO_NAME:latest $ECR_URI:latest
 docker push $ECR_URI:latest
 
